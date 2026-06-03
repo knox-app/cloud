25228c4d543f6c287789dc127b1e737550dcac8a
knox-app <xpertsdevlopers@gmail.com>
2026-06-03 19:31:10 +0530

    Create xprtfb.js

---
+++ keys/xprtfb.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDPm2za3Ho0Hd8Henuhq0KxAHyUQh-Hi-U",
+  authDomain: "question-metabase.firebaseapp.com",
+  databaseURL: "https://question-metabase-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "question-metabase",
+  storageBucket: "question-metabase.firebasestorage.app",
+  messagingSenderId: "770698198753",
+  appId: "1:770698198753:web:59ae49d78b6013bab5c1a0",
+  measurementId: "G-6ZP1FPMKSJ"
+};
